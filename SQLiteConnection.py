97f884f332640b3953366f9150e1a44bc01c3c30
Flemming Christensen <flamekfor@gmail.com>
2022-04-26 20:16:37 +0200

    Added function for inserting data

--- SQLiteConnection.py
+++ SQLiteConnection.py
@@ -1,27 +1,43 @@
 import sqlite3
 
-# create/connect to db
-conn = sqlite3.connect("customer.db")
+def createTable() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
 
-# create cursor
-c = conn.cursor()
+    # create cursor
+    c = conn.cursor()
 
-# create a table
-c.execute("""CREATE TABLE customers (
-        first_name DATATYPE,
-        last_name DATATYPE,
-        email DATATYPE
-    )""")
+    # create a table
+    c.execute("""CREATE TABLE customers (
+            first_name DATATYPE,
+            last_name DATATYPE,
+            email DATATYPE
+        )""")
+    # DATATYPES in SQLite
+    # NULL
+    # INTEHER
+    # REAL
+    # TEXT
+    # BLOB mp3 mp4 video pictures....
 
-# DATATYPES in SQLite
-# NULL
-# INTEHER
-# REAL
-# TEXT
-# BLOB mp3 mp4 video pictures....
+    # Commit our command
+    conn.commit()
 
-# Commit our command
-conn.commit()
+    #Close our connection
+    conn.close()
 
-#Close our connection
-conn.close()
+def insertDatainDB() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
+
+    # create cursor
+    c = conn.cursor()
+
+    # create a table
+    c.execute("INSERT INTO customers VALUES" ('John','Elder','Johny@codemy.com'))
+
+    # Commit our command
+    conn.commit()
+
+    #Close our connection
+    conn.close()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-
+import SQLiteConnection
 
 def main():
     print("Hello World!")
+    SQLiteConnection.createTable()
+    SQLiteConnection.insertDatainDB()
 
 if __name__ == "__main__":
     main()
