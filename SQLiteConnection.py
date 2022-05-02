8d53d6df60ae0ec973becb03772199d1a2c26416
Flemming Christensen <flamekfor@gmail.com>
2022-05-02 23:37:37 +0200

    Update and delete

--- SQLiteConnection.py
+++ SQLiteConnection.py
@@ -63,3 +63,125 @@ def insertManyDatainDB() -> None:
 
     #Close our connection
     conn.close()
+
+def QueryTheDataBase() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
+
+    # create cursor
+    c = conn.cursor()
+
+    # Query the Database
+    c.execute("SELECT * FROM customers")
+    #c.fetchone()
+    #c.fetchmany(3)
+    #c.fetchall()
+    print(c.fetchall())
+
+    conn.commit()
+
+    c.close()
+
+def QuerySearchTheDataBase() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
+
+    # create cursor
+    c = conn.cursor()
+
+    # Query the Database
+    c.execute("SELECT * FROM customers")
+
+    items = c.fetchall()
+
+    for item in items:
+        print(item[0])
+
+    conn.commit()
+
+    c.close()
+
+def QueryPrimaryKeyDataBase() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
+
+    # create cursor
+    c = conn.cursor()
+
+    # Query the Database with Primary key
+    # SQLite automatically add rowID's for each record "set of data"
+    c.execute("SELECT rowid,* FROM customers")
+
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+
+    conn.commit()
+
+    c.close()
+
+def SearchSpecificThinginTheDataBase() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
+
+    # create cursor
+    c = conn.cursor()
+
+    # Query the Database with Primary key
+    # SQLite automatically add rowID's for each record "set of data"
+    c.execute("SELECT * FROM customers WHERE last_name = 'Mikkel'")
+
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+
+    conn.commit()
+
+    c.close()
+
+def UpdateRecordsInTheDataBase() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
+
+    # create cursor
+    c = conn.cursor()
+
+    # Update Record
+    c.execute("""UPDATE customers SET first_name = 'Bob' 
+                 WHERE rowid = 1    
+    """)
+
+    conn.commit()
+
+    c.execute("SELECT rowid,* FROM customers")
+
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+
+    conn.commit()
+
+    c.close()
+
+def DeleteRecordsInTheDataBase() -> None:
+    # create/connect to db
+    conn = sqlite3.connect("customer.db")
+
+    # create cursor
+    c = conn.cursor()
+
+    # Query the Database with Primary key
+    # SQLite automatically add rowID's for each record "set of data"
+    c.execute("DELETE FROM customers WHERE rowid = 6")
+
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+
+    conn.commit()
+
+    c.close()
--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import SQLiteConnection
 def main():
     print("Hello World!")
     #SQLiteConnection.createTable()
-    SQLiteConnection.insertDatainDB()
-    SQLiteConnection.insertManyDatainDB()
+    #SQLiteConnection.insertDatainDB()
+    #SQLiteConnection.insertManyDatainDB()
+    #SQLiteConnection.QueryTheDataBase()
+    #SQLiteConnection.QuerySearchTheDataBase()
+    #SQLiteConnection.QueryPrimaryKeyDataBase()
+    #SQLiteConnection.SearchSpecificThinginTheDataBase()
+    SQLiteConnection.UpdateRecordsInTheDataBase()
+    SQLiteConnection.DeleteRecordsInTheDataBase()
+
 
 if __name__ == "__main__":
     main()
